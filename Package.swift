156be9d0b258fedd45d8c4c2b49c73f737fc7035
MirzaDhanuSulistyo <35160617+MirzaDhanuSulistyo@users.noreply.github.com>
2025-04-22 12:56:39 +0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "FlagPhoneNumber",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "FlagPhoneNumber",
+            targets: ["FlagPhoneNumber"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "FlagPhoneNumber",
+            path: "Sources"
+        )
+    ]
+)
